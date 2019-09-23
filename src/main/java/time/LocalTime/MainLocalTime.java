46b6a9d87e41622018d057be07754931d16ad6b4
Juan Bonilla <juan.bonilla@gorillalogic.com>
2019-09-23 10:13:06 -0600

    LocalTime Examples

---
+++ src/main/java/time/LocalTime/MainLocalTime.java
@@ -0,0 +1,21 @@
+package time.LocalTime;
+
+/**
+ * Module java.base
+ * Package java.time
+ * https://docs.oracle.com/javase/8/docs/api/java/time/LocalTime.html
+ */
+
+public class MainLocalTime {
+
+    public static void main(String[] args) {
+
+        LocalTimeExamples localTimeExamples = new LocalTimeExamples();
+
+        localTimeExamples.basicExamples();
+
+        localTimeExamples.immutable();
+
+    }
+
+}
