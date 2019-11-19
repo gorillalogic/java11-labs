81810a3d1063716081ed236e02093c58e33b366d
Shiradit Moya Leal <shiradit.moya@gorillalogic.com>
2019-11-19 10:01:45 -0500

    Creating workshop of java learning from scratch.

---
+++ src/com/gorillalogic/java/starter/cl1/Greetings.java
@@ -0,0 +1,7 @@
+package com.gorillalogic.java.starter.cl1;
+
+public class Greetings {
+    public static void main(String[] args) {
+        System.out.println("Hello Java, this is "+args[0]);
+    }
+}
