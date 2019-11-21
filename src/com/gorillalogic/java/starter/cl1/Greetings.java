41c5af9697a9b75a215db1cf25fb3871e9a8e478
Shiradit Moya <shiradit.moya@gorillalogic.com>
2019-11-21 09:23:53 -0500

    Fixing Java src

--- src/com/gorillalogic/java/starter/cl1/Greetings.java
+++
@@ -1,7 +0,0 @@
-package com.gorillalogic.java.starter.cl1;
-
-public class Greetings {
-    public static void main(String[] args) {
-        System.out.println("Hello Java, this is "+args[0]);
-    }
-}
