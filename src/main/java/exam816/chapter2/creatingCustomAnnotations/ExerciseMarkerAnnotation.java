3aa73ba23dfa850326ca30b58ef3d0c373299411
diniamendezGL <welcome2GL>
2020-09-28 08:16:18 -0600

    Chapter 2 - Annotations

---
+++ src/main/java/exam816/chapter2/creatingCustomAnnotations/ExerciseMarkerAnnotation.java
@@ -0,0 +1,9 @@
+package exam816.chapter2.creatingCustomAnnotations;
+
+/**
+ * The Exercise annotation is referred to as a marker annotation,
+ * since it does not contain any elements.
+ */
+public @interface ExerciseMarkerAnnotation {
+
+}
