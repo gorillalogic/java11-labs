0a60074082c577a50d77a9ee68564e28b8936cc3
Valery Méndez <dinia.mendez@gorillalogic.com>
2020-10-10 10:19:00 -0600

    Chapter 2 - Add Example to import a constant from an annotation

--- src/main/java/exam816/chapter2/elementTypes2/Bear.java
+++ src/main/java/exam816/chapter2/elementTypes2/Bear.java
@@ -1,4 +1,8 @@
 package exam816.chapter2.elementTypes2;
 
+import static exam816.chapter2.elementTypes2.constantVariables.ElectricitySource.MIN_VOLTAGE;
+
 public class Bear {
+    //Example to import a constant from an annotation
+    int test = MIN_VOLTAGE;
 }
